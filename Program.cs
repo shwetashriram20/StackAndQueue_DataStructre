dc9c142d19aa44aa33e152a6652c5715e57acfd7
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-11 07:44:45 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,18 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Stack obj = new Stack();
-            Console.WriteLine("After push the elements are : ");
 
-            obj.push(70);
-            obj.push(30);
-            obj.push(56);
-            Console.WriteLine();
-            Console.WriteLine("Item popped from stack = " + obj.pop());
-
-
-            Console.WriteLine("Top Element of the stack is : " + obj.peek());
-
+          Console.WriteLine("Stacl and Queue: ");
 
         }
     }
