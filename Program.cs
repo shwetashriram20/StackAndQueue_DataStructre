e47213dbb67f6249e7529d528a4b59ba52159a3c
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-11 07:22:56 +0530

    method

--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Stack And Queue");
+            Stack obj = new Stack();
+            Console.WriteLine("After push the elements are : ");
+
+            obj.push(70);
+            obj.push(30);
+            obj.push(56);
+            Console.WriteLine();
+
+            Console.WriteLine("Top Element of the stack is : " + obj.peek());
+
 
         }
     }
